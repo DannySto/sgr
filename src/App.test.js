0fd60378585e8d3d01ea8cc08aeb50d638406f50
DannySto <dannyst@zoho.eu>
2023-12-11 21:00:53 +0100

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders sgr link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/SOLID GROOVE RECORDS/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('loads sgr image', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/SOLID GROOVE RECORDS/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('verify mail icon', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/SOLID GROOVE RECORDS/i);
   expect(linkElement).toBeInTheDocument();
 });
