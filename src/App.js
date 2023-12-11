a08d35f5b982d7c75ebf99085c83e821d99073d3
DannySto <dannyst@zoho.eu>
2023-12-11 23:46:13 +0100

    deploy react page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './front.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       - Solid Groove Records -
+    //     </p>
+    //   </header>
+    // </div>
+    <div className="content">
+      <header>
+        <img src="sgr.jpg"  alt="Solid Groove Records" id="avatar"></img>
+        <h1>SOLID GROOVE RECORDS</h1>
+        <ul>
+          <li><a class="fa fa-2x fa-facebook" href="https://facebook.com/solidgrooverecords"><span class="description">Facebook</span></a></li>
+          <li><a class="fa fa-2x fa-steam" href="https://www.beatport.com/label/solid-groove-records/36628"><span class="description">Beatport</span></a></li>
+          <li><a class="fa fa-2x fa-soundcloud" href="https://soundcloud.com/solid-groove-records"><span class="description">Soundcloud</span></a></li>
+          <li><a class="fa fa-2x fa-medium" href="https://www.junodownload.com/labels/Solid+Groove+Recordings"><span class="description">Junodownload</span></a></li>
+          <li><a class="fa fa-2x fa-weibo" href="https://www.traxsource.com/label/16347/solid-groove-records"><span class="description">Traxxsource</span></a></li>
+          <li><a class="fa fa-2x fa-envelope" href="mailto:demo@solidgrooverecords"><span class="description">Email</span></a></li>      
+        </ul>
       </header>
     </div>
   );
